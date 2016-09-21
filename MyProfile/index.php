b9e51bcf1b969b40bbef3746cf9d10bb260af69b
joezer BOURAIMA <jbouraima@karthsolutions.com>
2016-09-21 09:22:56 +0100

    création d la plateforme du group bouraima et des profiles du membre

---
+++ MyProfile/index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>BOURAIMA GROUPS</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../public/css/style.css" type="text/css">
+        <link rel="stylesheet" href="../public/vendor/bootstrap/dist/css/bootstrap-theme.css" type="text/css">
+        <link rel="stylesheet" href="../public/vendor/bootstrap/dist/css/bootstrap.css" type="text/css">
+        <link rel="stylesheet" href="../public/vendor/bootstrap/dist/css/bootstrap.css.map" type="text/css">
+        <link rel="stylesheet" href="../public/vendor/bootstrap/dist/css/bootstrap.min.css" type="text/css">
+    </head>
+    <body> 
+        <div class="container">
+            <div class="conteneur col-md-12">
+                <div class="col-md-12">
+                    <div class="col-md-3 logo">
+                        <img src="../public/img/logfamily.png" width="" height="100%">
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <nav class="navbar navbar-inverse">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <a class="navbar-brand" href=""><font color="#1b6d85"><b>BOURAIMA</b></font><font color="red"> GROUP</font></a>
+                            </div>
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="">Home</a></li>
+                                <li><a href="#">Page 1</a></li>
+                                <li><a href="#">Prestation</a></li> 
+                                <li><a href="#">Blog</a></li> 
+                                <li><a href="#">Contact</a></li> 
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <div class="col-md-12">
+                    <div class="row col-sm-12">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+
+                    </div>
+                    <div class="row col-sm-12">
+                        <div class="col-md-8 left_bloc">
+                            <div class="form-group marge">
+                                <div class="col-sm-12 cadre_bloc">
+                                    <div class="col-sm-4 profil_bloc">
+                                        <img src="../public/img/gerard.jpg"  class="img-responsive" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <span class="col-sm-12"><a href="profile.php">RAIMI F. Gérard Chef comptable</a></span>
+                                        <span>&nbsp;</span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div>&nbsp;</div>
+                            <div class="form-group marge">
+                                <div class="col-sm-12 cadre_bloc">
+                                    <div class="col-sm-4 profil_bloc">
+                                        <img src="../public/img/prof.jpg"  class="img-responsive" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <span class="col-sm-12"><a href="">BOURAIMA A. Joezer Ingénieur analyste programmeur</a></span>
+                                        <span>&nbsp;</span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div>&nbsp;</div>
+                            <div class="form-group">
+                                <div class="col-sm-12 cadre_bloc">
+                                    <div class="col-sm-4 profil_bloc">
+                                        <img src="../public/img/femi.jpg"  class="img-responsive" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <span class="col-sm-12"><a href="">BOURAIMA I. Oluwafèmi Ingénieur réseau et télécom</a></span>
+                                        <span>&nbsp;</span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div>&nbsp;</div>
+                            <div class="form-group">
+                                <div class="col-sm-12 cadre_bloc">
+                                    <div class="col-sm-4 profil_bloc">
+                                        <img src="../public/img/aaron.jpg"  class="img-responsive" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <span class="col-sm-12"><a href="">BOURAIMA O. Aaron Entrepreneur du groupe NCI</a></span>
+                                        <span>&nbsp;</span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div>&nbsp;</div>
+                            <div class="form-group">
+                                <div class="col-sm-12 cadre_bloc">
+                                    <div class="col-sm-4 profil_bloc">
+                                        <img src="../public/img/toyin.jpg"  class="img-responsive" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <span class="col-sm-12"><a href="">BOURAIMA O. Nehemi Mecanicien auto: chef garage</a></span>
+                                        <span>&nbsp;</span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div>&nbsp;</div>
+                            <div class="form-group">
+                                <div class="col-sm-12 cadre_bloc">
+                                    <div class="col-sm-4 profil_bloc">
+                                        <img src="../public/img/foumi.jpg"  class="img-responsive" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <span class="col-sm-12"><a href="">BOURAIMA F. Estelle  Transport logistique</a></span>
+                                        <span>&nbsp;</span>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div>&nbsp;</div>
+                        </div>
+                        <div class="col-md-offset-1 col-md-3 right_bloc">
+                            <div class="row form-group pad_bloc">
+                                <div class="row bar_pub">
+                                    SERVICE
+                                </div>
+                                <div class="row pub_bloc">
+                                    
+                                </div>
+                            </div>
+                            <div class="clearfix">&nbsp;</div>
+                            <div class="row form-group">
+                                <div class="row bar_pub">
+                                    PUBLICITE
+                                </div>
+                                <div class="row pub_bloc">
+                                    
+                                </div>
+                            </div>
+                            <div class="clearfix">&nbsp;</div>
+                            <div class="row form-group">
+                                <div class="row bar_pub">
+                                    PARTENAIRE
+                                </div>
+                                <div class="row pub_bloc">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
---
+++ MyProfile/profile.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Description du profile</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../public/css/style.css" type="text/css">
+        <link rel="stylesheet" href="../public/vendor/bootstrap/dist/css/bootstrap-theme.css" type="text/css">
+        <link rel="stylesheet" href="../public/vendor/bootstrap/dist/css/bootstrap.css" type="text/css">
+        <link rel="stylesheet" href="../public/vendor/bootstrap/dist/css/bootstrap.css.map" type="text/css">
+        <link rel="stylesheet" href="../public/vendor/bootstrap/dist/css/bootstrap.min.css" type="text/css">
+    </head>
+    <body> 
+        <div class="container">
+            <div class="conteneur col-md-12">
+                 <div class="col-md-12">
+                    <div class="col-md-3 logo">
+                        <img src="../public/img/logfamily.png" width="" height="100%">
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <nav class="navbar navbar-inverse">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <a class="navbar-brand" href="#"><font color="#1b6d85"><b>BOURAIMA</b></font><font color="red"> GROUP</font></a>
+                            </div>
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="index.php">Home</a></li>
+                                <li><a href="#">Page 1</a></li>
+                                <li><a href="#">Prestation</a></li> 
+                                <li><a href="#">Blog</a></li> 
+                                <li><a href="#">Contact</a></li> 
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <div class="col-md-12">
+                    <div class="row col-sm-12">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+
+                    </div>
+                    <div class="row col-sm-12">
+                        <div class="col-md-8 left_bloc">
+                            <div class="col-md-5 profile">
+                                 <img src="../public/img/prof.jpg"  class="img-responsive" width="100%" height="100%">
+                            </div>
+                            <div class="col-md-offset-1 col-md-6 header-profile">
+                                <p>
+                                    <h3>RAIMI F.Gerard</h3><br>
+                                    <h5>Poste actuel: Chef comptable</h5> <br>
+                                    <h5>Niveau d'étude: BAC + 5</h5><br>
+                                    <h5>Espérience professionelle: 4 ans</h5><br>
+                                    <font color='skyblue'><h5>Contact: +229 97096632 /steevenraimi@gmail</h5></font>
+                                </p>
+                            </div>
+                            <div class="clearfix">&nbsp;</div>
+                            <div class="col-md-12 description-profile">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </p>
+                                <p>
+                                    Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.
+                                </p>
+                                <p>
+                                    Fusce convallis, mauris imperdiet gravida bibendum, nisl turpis suscipit mauris, sed placerat ipsum urna sed risus. In convallis tellus a mauris. Curabitur non elit ut libero tristique sodales. Mauris a lacus. Donec mattis semper leo. In hac habitasse platea dictumst. Vivamus facilisis diam at odio. Mauris dictum, nisi eget consequat elementum, lacus ligula molestie metus, non feugiat orci magna ac sem. Donec turpis. Donec vitae metus. Morbi tristique neque eu mauris. Quisque gravida ipsum non sapien. Proin turpis lacus, scelerisque vitae, elementum at, lobortis ac, quam. Aliquam dictum eleifend risus. In hac habitasse platea dictumst. Etiam sit amet diam. Suspendisse odio. Suspendisse nunc. In semper bibendum libero.
+                                </p>
+                                <p>
+                                    Proin nonummy, lacus eget pulvinar lacinia, pede felis dignissim leo, vitae tristique magna lacus sit amet eros. Nullam ornare. Praesent odio ligula, dapibus sed, tincidunt eget, dictum ac, nibh. Nam quis lacus. Nunc eleifend molestie velit. Morbi lobortis quam eu velit. Donec euismod vestibulum massa. Donec non lectus. Aliquam commodo lacus sit amet nulla. Cras dignissim elit et augue. Nullam non diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Aenean vestibulum. Sed lobortis elit quis lectus. Nunc sed lacus at augue bibendum dapibus.
+                                </p>
+
+                            </div>
+                        </div>
+                        <div class="col-md-offset-1 col-md-3 right_bloc">
+                            <div class="row form-group pad_bloc">
+                                <div class="row bar_pub">
+                                    SERVICE
+                                </div>
+                                <div class="row pub_bloc">
+
+                                </div>
+                            </div>
+                            <div class="clearfix">&nbsp;</div>
+                            <div class="row form-group">
+                                <div class="row bar_pub">
+                                    PUBLICITE
+                                </div>
+                                <div class="row pub_bloc">
+
+                                </div>
+                            </div>
+                            <div class="clearfix">&nbsp;</div>
+                            <div class="row form-group">
+                                <div class="row bar_pub">
+                                    PARTENAIRE
+                                </div>
+                                <div class="row pub_bloc">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
