37df4bf381bde06cedb28a575f9ff36463dcbb68
joezer BOURAIMA <jbouraima@karthsolutions.com>
2016-11-18 14:44:49 +0100

    ajout du détail produit des services transit et location

--- Ets/footer.php
+++ Ets/footer.php
@@ -1,124 +1,154 @@
 </div>
 <div class="clearfix">&nbsp;</div>
-        <div class="row col-md-12 col-xs-12">
-            <div class="col-xs-12 col-md-12 col-sm-12 transit">
-                <span class="service"><b>SERVICE VENTE DE VEHICULE D'OCCASION</b></span>
-                <div class="col-md-12 promotion-vehicule">
-                    Promotion des v&eacute;hicules d&apos;occasion 
+<div class="row col-md-12 col-xs-12">
+    <div class="col-xs-12 col-md-12 col-sm-12 transit">
+        <span class="service"><b>SERVICE VENTE DE VEHICULE D'OCCASION</b></span>
+        <div class="col-md-12 promotion-vehicule">
+            Promotion des v&eacute;hicules d&apos;occasion 
+        </div>
+        <div class="col-md-12 cadre-transit">
+            <div class="carousel slide"  id="carousel2">
+                <div class="carousel-inner">
+                    <div class="item active">
+
+                        <div class="col-md-3 bloc-vehicule">
+                            <div class="col-md-12 col-xs-12 profil-vehicule">
+                                <a href="services-all.php"><img class="img-responsive" src="../public/img/vehicule/merco.jpg" width="100%" height="100%"></a> 
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <p>Marque: Mercedez-benz</p>
+                                2.000000 F CFA
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <a href="#" class="btn btn-default btn-sm">Details >></a>
+                            </div>
+                        </div>
+                        <div class="col-md-3 bloc-vehicule">
+                            <div class="col-md-12 col-xs-12 profil-vehicule">
+                                <a href="#"><img class="img-responsive" src="../public/img/vehicule/lexus.jpg" width="100%" height="100%"></a> 
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <p>Marque: Lexus</p>
+                                2.000000 F CFA
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <a href="#" class="btn btn-default btn-sm">Details >></a>
+                            </div>
+                        </div>
+                        <div class="col-md-3 bloc-vehicule">
+                            <div class="col-md-12 col-xs-12 profil-vehicule">
+                                <a href="#"><img class="img-responsive" src="../public/img/vehicule/honda.jpg" width="100%" height="100%"></a> 
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <p>Marque: honda</p>
+                                2.000000 F CFA
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <a href="#" class="btn btn-default btn-sm">Details >></a>
+                            </div>
+                        </div>
+                        <div class="col-md-3 bloc-vehicule">
+                            <div class="col-md-12 col-xs-12 profil-vehicule">
+                                <a href="#"><img class="img-responsive" src="../public/img/vehicule/bmw.jpg" width="100%" height="100%"></a> 
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <p>Marque: BMW</p>
+                                2.000000 F CFA
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <a href="#" class="btn btn-default btn-sm">Details >></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-12 cadre-transit">
-                    <div class="col-md-3 bloc-vehicule">
-                        <div class="col-md-12 col-xs-12 profil-vehicule">
-                            <img class="img-responsive" src="../public/img/vehicule/merco.jpg" width="100%" height="100%"> 
+                <a data-slide="prev" href="#carousel2" class="left carousel-control" style="left:-40px;">
+                    <span class="icon-prev"></span>
+                </a>
+                <a data-slide="next" href="#carousel2" class="right carousel-control" style="right:-40px;">
+                    <span class="icon-next "></span>
+                </a>
+            </div>                    
+
+        </div>
+    </div>
+</div>
+<div class="clearfix">&nbsp;</div>
+<div class="clearfix">&nbsp;</div>
+<div class="clearfix">&nbsp;</div>
+<div class="row col-md-12">
+    
+    <div class="col-xs-12 col-md-12 col-sm-12 transit">
+        <span class="service"><b>SERVICE IMMOBILIER</b></span>
+        <div class="col-md-12 promotion-vehicule">
+            Publicit&eacute; des locations d'appartement 
+        </div>
+        <div class="col-md-12 cadre-transit">
+
+            <div class="carousel slide"  id="carousel3">
+                <div class="carousel-inner">
+                    <div class="item active">
+
+                        <div class="col-md-3 bloc-vehicule">
+                            <div class="col-md-12 col-xs-12 profil-vehicule">
+                                <a href="#"> <img class="img-responsive" src="../public/img/immobilier/b.jpg" width="100%" height="100%"></a> 
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <p>Villa complet</p>
+                                2.000000 F CFA
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <a href class="btn btn-default btn-sm">Details >></a>
+                            </div>
                         </div>
-                        <div class="col-md-12 col-xs-12">
-                            <p>Marque: Mercedez-benz</p>
-                            2.000000 F CFA
+                        <div class="col-md-3 bloc-vehicule">
+                            <div class="col-md-12 col-xs-12 profil-vehicule">
+                                <a href="#"><img class="img-responsive" src="../public/img/immobilier/c.jpg" width="100%" height="100%"></a> 
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <p>Villa complet</p>
+                                2.000000 F CFA
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <a href="#" class="btn btn-default btn-sm">Details >></a>
+                            </div>
                         </div>
-                        <div class="col-md-12 col-xs-12">
-                            <button class="btn btn-default btn-sm">Details >></button>
+                        <div class="col-md-3 bloc-vehicule">
+                            <div class="col-md-12 col-xs-12 profil-vehicule">
+                                <a href="#"> <img class="img-responsive" src="../public/img/immobilier/d.jpg" width="100%" height="100%"></a>
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <p>Villa complet</p>
+                                2.000000 F CFA
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <a href="#" class="btn btn-default btn-sm">Details >></a>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-3 bloc-vehicule">
-                        <div class="col-md-12 col-xs-12 profil-vehicule">
-                            <img class="img-responsive" src="../public/img/vehicule/lexus.jpg" width="100%" height="100%"> 
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <p>Marque: Lexus</p>
-                            2.000000 F CFA
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <button class="btn btn-default btn-sm">Details >></button>
-                        </div>
-                    </div>
-                    <div class="col-md-3 bloc-vehicule">
-                        <div class="col-md-12 col-xs-12 profil-vehicule">
-                            <img class="img-responsive" src="../public/img/vehicule/honda.jpg" width="100%" height="100%"> 
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <p>Marque: honda</p>
-                            2.000000 F CFA
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <button class="btn btn-default btn-sm">Details >></button>
-                        </div>
-                    </div>
-                    <div class="col-md-3 bloc-vehicule">
-                        <div class="col-md-12 col-xs-12 profil-vehicule">
-                            <img class="img-responsive" src="../public/img/vehicule/bmw.jpg" width="100%" height="100%"> 
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <p>Marque: BMW</p>
-                            2.000000 F CFA
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <button class="btn btn-default btn-sm">Details >></button>
+                        <div class="col-md-3 bloc-vehicule">
+                            <div class="col-md-12 col-xs-12 profil-vehicule">
+                                <a href="#"><img class="img-responsive" src="../public/img/immobilier/e.jpg" width="100%" height="100%"></a> 
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <p>Villa complet</p>
+                                2.000000 F CFA
+                            </div>
+                            <div class="col-md-12 col-xs-12">
+                                <a href="#" class="btn btn-default btn-sm">Details >></a>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <a data-slide="prev" href="#carousel3" class="left carousel-control" style="left:-40px;">
+                    <span class="icon-prev"></span>
+                </a>
+                <a data-slide="next" href="#carousel3" class="right carousel-control" style="right:-40px;">
+                    <span class="icon-next "></span>
+                </a>
             </div>
         </div>
-<div class="clearfix">&nbsp;</div>
-<div class="clearfix">&nbsp;</div>
-<div class="clearfix">&nbsp;</div>
-        <div class="row">
-            <div class="col-xs-12 col-md-12 col-sm-12 transit">
-                <span class="service"><b>SERVICE IMMOBILIER</b></span>
-                <div class="col-md-12 promotion-vehicule">
-                    Publicit&eacute; des locations d'appartement 
-                </div>
-                <div class="col-md-12 cadre-transit">
-                    <div class="col-md-3 bloc-vehicule">
-                        <div class="col-md-12 col-xs-12 profil-vehicule">
-                            <img class="img-responsive" src="../public/img/immobilier/b.jpg" width="100%" height="100%"> 
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <p>Villa complet</p>
-                            2.000000 F CFA
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <button class="btn btn-default btn-sm">Details >></button>
-                        </div>
-                    </div>
-                    <div class="col-md-3 bloc-vehicule">
-                        <div class="col-md-12 col-xs-12 profil-vehicule">
-                            <img class="img-responsive" src="../public/img/immobilier/c.jpg" width="100%" height="100%"> 
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <p>Villa complet</p>
-                            2.000000 F CFA
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <button class="btn btn-default btn-sm">Details >></button>
-                        </div>
-                    </div>
-                    <div class="col-md-3 bloc-vehicule">
-                        <div class="col-md-12 col-xs-12 profil-vehicule">
-                            <img class="img-responsive" src="../public/img/immobilier/d.jpg" width="100%" height="100%"> 
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <p>Villa complet</p>
-                            2.000000 F CFA
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <button class="btn btn-default btn-sm">Details >></button>
-                        </div>
-                    </div>
-                    <div class="col-md-3 bloc-vehicule">
-                        <div class="col-md-12 col-xs-12 profil-vehicule">
-                            <img class="img-responsive" src="../public/img/immobilier/e.jpg" width="100%" height="100%"> 
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <p>Villa complet</p>
-                            2.000000 F CFA
-                        </div>
-                        <div class="col-md-12 col-xs-12">
-                            <button class="btn btn-default btn-sm">Details >></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    </div>
+    
+</div>
 <div class="clearfix">&nbsp;</div>
 
 <div class="clearfix">&nbsp;</div>
@@ -178,12 +208,12 @@
 <script type="text/javascript" src="../public/popup/js/mvpready-helpers.js"></script>
 <script type="text/javascript" src="../public/popup/js/mvpready-admin.js"></script>
 <script>
-   /* $(document).ready(function () {
-        $(".nav a").on("click", function () {
-            $(".nav").find(".active").removeClass("active");
-            $(this).parent().addClass("active");
-        });
-
-    });*/
+    /* $(document).ready(function () {
+     $(".nav a").on("click", function () {
+     $(".nav").find(".active").removeClass("active");
+     $(this).parent().addClass("active");
+     });
+     
+     });*/
 </script>
 </html>
---
+++ Ets/services-all.php
@@ -0,0 +1,275 @@
+<?php
+include './header.php';
+include './requete.php';
+
+?>
+<div class="col-md-12 col-xs-12 col-lg-12">
+    <div class="col-md-7 produit-profile">
+        <img src="../public/img/vehicule/lexus.jpg" width="100%" height="100%" class="img-responsive">
+    </div>
+    <div class="col-md-4 col-md-offset-1 detail-produit">
+        <span class="bar-detail-produit row container col-md-12">D&eacute;tails du produit du produit</span>
+        <div>
+            <span class="col-md-12">&nbsp;</span>
+            <p>
+                <b> Marque: </b> Toyota <br>
+                <b> Ann&eacute;e: </b> 2008 <br>
+                <b> Disque:</b> 6 Cylindre <br>
+                <b> Moteur:</b> Diesel <br>
+                <b> Couleur:</b> grise <br>
+                <b> D&eacute;mare:</b> oui <br>
+                <b> prix:</b> 3.000.000 F CFA <br>
+                <br>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            </p>
+        </div>
+    </div>
+    <div class="col-md-12 cadre-transit">
+        <div class="carousel slide"  id="carousel2">
+            <div class="carousel-inner">
+                <div class="item active">
+
+                    <div class="col-md-3 bloc-vehicule">
+                        <div class="col-md-12 col-xs-12 profil-vehicule">
+                            <a href="services-all.php"><img class="img-responsive" src="../public/img/vehicule/merco.jpg" width="100%" height="100%"></a> 
+                        </div>
+                    </div>
+                    <div class="col-md-3 bloc-vehicule">
+                        <div class="col-md-12 col-xs-12 profil-vehicule">
+                            <a href="#"><img class="img-responsive" src="../public/img/vehicule/lexus.jpg" width="100%" height="100%"></a> 
+                        </div>
+                    </div>
+                    <div class="col-md-3 bloc-vehicule">
+                        <div class="col-md-12 col-xs-12 profil-vehicule">
+                            <a href="#"><img class="img-responsive" src="../public/img/vehicule/honda.jpg" width="100%" height="100%"></a> 
+                        </div>
+                    </div>
+                    <div class="col-md-3 bloc-vehicule">
+                        <div class="col-md-12 col-xs-12 profil-vehicule">
+                            <a href="#"><img class="img-responsive" src="../public/img/vehicule/bmw.jpg" width="100%" height="100%"></a> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a data-slide="prev" href="#carousel2" class="left carousel-control" style="left:-40px;">
+                <span class="icon-prev"></span>
+            </a>
+            <a data-slide="next" href="#carousel2" class="right carousel-control" style="right:-40px;">
+                <span class="icon-next "></span>
+            </a>
+        </div>                    
+    </div>
+</div>
+<div class="band col-md-12"></div>
+<div class="clearfix">&nbsp;</div>
+<div class="col-md-12 cadre-transit">
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="services-all.php"><img class="img-responsive" src="../public/img/vehicule/merco.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: Mercedez-benz</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/lexus.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: Lexus</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/honda.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: honda</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/bmw.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: BMW</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+</div>
+
+<div class="clearfix">&nbsp;</div>
+
+<div class="col-md-12 cadre-transit">
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="services-all.php"><img class="img-responsive" src="../public/img/vehicule/merco.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: Mercedez-benz</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/lexus.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: Lexus</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/honda.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: honda</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/bmw.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: BMW</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+</div>
+<div class="clearfix">&nbsp;</div>
+<div class="col-md-12 cadre-transit">
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="services-all.php"><img class="img-responsive" src="../public/img/vehicule/merco.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: Mercedez-benz</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/lexus.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: Lexus</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/honda.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: honda</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/bmw.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: BMW</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+</div>
+<div class="clearfix">&nbsp;</div>
+<div class="col-md-12 cadre-transit">
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="services-all.php"><img class="img-responsive" src="../public/img/vehicule/merco.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: Mercedez-benz</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/lexus.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: Lexus</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/honda.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: honda</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+    <div class="col-md-3 bloc-vehicule">
+        <div class="col-md-12 col-xs-12 profil-vehicule">
+            <a href="#"><img class="img-responsive" src="../public/img/vehicule/bmw.jpg" width="100%" height="100%"></a> 
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <p>Marque: BMW</p>
+            2.000000 F CFA
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <a href="#" class="btn btn-default btn-sm">Details >></a>
+        </div>
+    </div>
+</div>
+
+
+<?php include './footer.php'; ?>
